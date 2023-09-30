b9950c51b2ad5e2972a704b56bf485da213110f4
Viraj Kaushalye <virajkaush@gmail.com>
2023-09-30 12:10:57 +0530

    basic start

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  bracketSameLine: true,
+  arrowParens: 'avoid',
+  plugins: [
+    require('prettier-plugin-tailwindcss'),
+  ],
+}
